b0009a0328528981c5b1835de17a21bfc7d8114e
James Singleton <jamesrsingleton1@gmail.com>
2024-02-01 20:32:55 -0700

    chore: more setup

---
+++ apps/web/app/(admin)/admin/page.tsx
@@ -0,0 +1,7 @@
+export default function DashboardPage() {
+  return (
+    <div className="flex flex-1 flex-col gap-4 p-4 md:gap-8 md:p-6">
+      <h1 className="text-lg font-semibold md:text-2xl">Dashboard</h1>
+    </div>
+  );
+}
